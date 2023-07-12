bef19a6981130373d7dea67a12fd1fbb2ebd16da
overkam <superivan0755@gmail.com>
2023-07-12 17:43:37 +0400

    get log back

--- src/wallet-connect/index.ts
+++ src/wallet-connect/index.ts
@@ -34,6 +34,7 @@ export class WalletsConnect extends AbstractConnector {
       this.connector = await EthereumProvider.init({
         ...provider.provider[provider.useProvider].wcConfig,
       });
+      console.log('this.connector', this.connector);
 
       if (!this.connector.session?.pairingTopic) {
         await this.connector.connect();
